6fe7f401152b87aeb96fe7cb6208785e4b3a0ce0
DarrenN <info@v25media.com>
2019-02-17 17:04:01 -0500

    Move to old-skool vector-set! and vector-copy

--- sixth.rkt
+++ sixth.rkt
@@ -1,20 +1,16 @@
 #lang racket
 
-(require 2htdp/image
-         2htdp/universe
+(require 2htdp/universe
          data/ring-buffer
          openssl/sha1
+         racket/draw
          racket/function
          racket/list
          racket/vector)
 
 (define WIDTH 1020)
 (define HEIGHT 600)
-(define CELL 10) ;; giant blocks for raspi
-(define CELL-CENTER (/ CELL 2))
-
-(define CANVAS (freeze (rectangle WIDTH HEIGHT "solid" "white")))
-(define SQUARE (freeze (square CELL "solid" "black")))
+(define CELL 5) ;; giant blocks for raspi
 
 (define WIDTH-RANGE (/ WIDTH CELL))
 (define HEIGHT-RANGE (/ HEIGHT CELL))
@@ -31,8 +27,6 @@
 
 (define OUTP (open-output-string))
 
-(define SHOW-LABELS #f)
-
 (define rb (empty-ring-buffer 3))
 
 ;; [Effect]
@@ -45,11 +39,14 @@
 ;; (-> vector?)
 ;; Pre-load a vector of vectors with ALIVE/DEAD
 (define (make-random-cells)
-  (for/vector #:length HEIGHT-RANGE ([y Y-AXIS])
-    (for/vector #:length WIDTH-RANGE ([x X-AXIS])
-      (if (zero? (modulo (random 11) 10))
-          (alive (* CELL x) (* CELL y))
-          (dead (* CELL x) (* CELL y))))))
+  (build-vector
+   HEIGHT-RANGE
+   (λ (y)
+     (build-vector WIDTH-RANGE
+                   (λ (x) (if (zero? (random 3))
+                              (dead (* CELL x) (* CELL y))
+                              (alive (* CELL x) (* CELL y))))))))
+
 
 ;; (-> real? real? vector? real?)
 ;; Return cell-state value for cell at x y but allow to wrap around edges
@@ -90,20 +87,25 @@
 ;; (-> vector? vector? real? vector?)
 ;; Loop over the row and for each cell get the sum of its neighbors and
 ;; decide if the cell lives (1) or dies (0) based on that sum.
-(define (apply-rules cells row y)
-  (for/vector #:length WIDTH ([x X-AXIS])
-    (define cell (vector-ref row x))
+(define (apply-rules old-cells old-row y)
+  (define new-row (vector-copy old-row))
+  (for ([cell (in-vector old-row)]
+        [x X-AXIS])
     (write (if (alive? cell) 1 0) OUTP)
-    (define neighbors (get-neighbors x y cells))
+    (define neighbors (get-neighbors x y old-cells))
     (define new-x (* CELL x))
     (define new-y (* CELL y))
     (cond
-      [(and (dead? cell) (= neighbors 3)) (alive new-x new-y)]
-      [(and (alive? cell) (< neighbors 2)) (dead new-x new-y)]
+      [(and (dead? cell) (= neighbors 3))
+       (vector-set! new-row x (alive new-x new-y))]
+      [(and (alive? cell) (< neighbors 2))
+       (vector-set! new-row x (dead new-x new-y))]
       [(and (alive? cell) (or (= neighbors 2) (eq? neighbors 3)))
-       (alive new-x new-y)]
-      [(and (alive? cell) (> neighbors 3)) (dead new-x new-y)]
-      [else cell])))
+       (vector-set! new-row x (alive new-x new-y))]
+      [(and (alive? cell) (> neighbors 3))
+       (vector-set! new-row x (dead new-x new-y))]
+      [else (vector-set! new-row x cell)]))
+  new-row)
 
 (define (push-cell-state!)
   (ring-buffer-push!
@@ -112,27 +114,25 @@
         (bytes->string/utf-8
          (get-output-bytes OUTP #t) #\?)))))
 
-(define (create-state-labels)
-  (above
-   (text (ring-buffer-ref rb 0) 16 "red")
-   (text (ring-buffer-ref rb 1) 16 "blue")
-   (text (ring-buffer-ref rb 2) 16 "red")))
-
 ;; (-> vector? vector?)
 ;; On each tick zip over the vectors and apply rules, switching cell
 ;; state depending on neighbor conditions. Returns a new vector.
 (define (tick cells)
   ;; If we're in a stable oscillation then reset the buffer and scramble
   ;; the cells to "start over"
-  (define cs (if (check-buffer cells)
-                 (begin
-                   (reset-buffer!)
-                   (make-random-cells))
-                 cells))
+  (define old-cells (if (check-buffer cells)
+                        (begin
+                          (reset-buffer!)
+                          (make-random-cells))
+                        (vector-copy cells)))
 
-  (for/vector #:length HEIGHT ([row (in-vector cells)]
-                               [y Y-AXIS])
-    (apply-rules cs row y)))
+  (define new-cells (vector-copy old-cells))
+
+  (for ([row (in-vector old-cells)]
+        [y Y-AXIS])
+    (vector-set! new-cells y (apply-rules old-cells row y)))
+
+  new-cells)
 
 ;; [Effect]
 ;; (-> vector? bitmap?)
@@ -141,21 +141,19 @@
 (define (draw cells)
   (push-cell-state!)
 
-  (define new-image
-    (for*/fold ([canvas CANVAS])
-               ([y-index (in-vector HEIGHT-RANGE)]
-                [x-index (in-range WIDTH-RANGE)])
-      (define point (vector-ref (vector-ref cells y-index) x-index))
-      (if (alive? point)
-          (place-image SQUARE
-                       (+ (alive-x point) CELL-CENTER)
-                       (+ (alive-y point) CELL-CENTER)
-                       canvas)
-          canvas)))
+  (define target (make-bitmap WIDTH HEIGHT))
+  (define dc (new bitmap-dc% [bitmap target]))
+  (send dc set-brush "black" 'solid)
 
-  (cond
-    [SHOW-LABELS (place-image (create-state-labels) (/ WIDTH 2) 30 new-image)]
-    [else new-image]))
+  (for* ([y-index (in-range HEIGHT-RANGE)]
+         [x-index (in-range WIDTH-RANGE)])
+    (define cell (vector-ref (vector-ref cells y-index) x-index))
+    (when (alive? cell)
+      (send dc draw-rectangle
+            (alive-x cell) (alive-y cell)
+            CELL CELL)))
+
+  target)
 
 (define (check-buffer cells)
   (with-handlers ([exn:fail? (λ (e) #f)])
